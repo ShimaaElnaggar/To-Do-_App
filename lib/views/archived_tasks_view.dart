744d83fabbe5ff748c587ad213531255b9ab3ff1
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2024-01-16 20:14:40 +0200

    create ArchivedTasksView

---
+++ lib/views/archived_tasks_view.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class ArchivedTasksView extends StatelessWidget {
+  const ArchivedTasksView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text(" Archived Tasks"));
+  }
+}
