d3b480eeb3040ca5f519c4580f76217909758787
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2024-01-16 20:08:52 +0200

    create NewTaskView

---
+++ lib/views/new_task_view.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class NewTaskView extends StatelessWidget {
+  const NewTaskView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+        child: Text(" New Task "),
+    );
+  }
+}
