dac9d6c7eaf6670b4dcc5d73082c71cc9ab7ad52
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2024-01-16 20:05:09 +0200

    activate the Bottom Navigation Bar

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,9 +1,15 @@
 
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +23,14 @@ class HomeView extends StatelessWidget {
         onPressed: () {  },
       ),
       bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: const Color(0xff239B97),
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        onTap: (index){
+          setState(() {
+            currentIndex = index;
+          });
+        },
         items: const [
           BottomNavigationBarItem(
               icon: Icon(Icons.menu),
